0c5e950a64658105cb5faa9d089c8865e7abf47e
Callegher <claudiiaaraujo@gmail.com>
2023-12-13 22:25:20 -0400

    atulizando o exercicio

--- cod hotel.py
+++ cod hotel.py
@@ -1,7 +1,23 @@
-# Todos os numeros exceto 13 - for i in rage
+# Todos os numeros exceto 13 - ordem crescente - for i in rage
 numero = 0
 for i in range(20):
   numero = numero + 1
   if (numero == 13):
     continue
   print(numero)
+  
+# Todos os numeros exceto 13 - ordem decrescente - for i in rage
+for i in range(20, 0, -1):
+  if ( i == 13):
+    continue
+  print(i)
+
+# Todos os numeros exceto o 13 - laço while
+Contador = 1
+while (Contador <= 20):
+  if (Contador == 13):
+    Contador = Contador + 1
+    continue
+  else:
+    print(Contador)
+    Contador = Contador + 1
